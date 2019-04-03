c7c27dae681bba8eb6f0eb4f343f7bb6dd144bd8
ducaifeng86 <1613195189@qq.com>
2019-04-03 17:27:46 +0800

    初始化项目

---
+++ src/components/Layout.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<header>
+			<ul>
+				<li>退出</li>
+				<li>登录</li>
+				<li>注册</li>
+				<li>关于</li>
+			</ul>
+		</header>
+		<section>
+			<keep-alive>
+				<router-view></router-view>
+			</keep-alive>
+		</section>
+		<footer>
+			底部
+		</footer>
+	</div>
+</template>
+
+<script>
+	export default{
+		
+	}
+</script>
+
+<style>
+	
+</style>
